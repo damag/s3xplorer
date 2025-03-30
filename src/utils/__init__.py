c13e0cc7e62bcf91ea30653a6f8f8c06f61515f3
Damien Grassart <damien@grassart.com>
2025-03-30 18:33:23 +0200

    Update utils module exports for centralized access

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,10 @@
 """
-Utility functions for S3xplorer
-""" 
+Utility modules for the application.
+"""
+
+from src.utils.logging import get_logger
+from src.utils.config import get_config
+from src.utils.file_service import get_file_service
+from src.utils.theme import apply_theme, get_theme_manager
+
+__all__ = ['get_logger', 'get_config', 'get_file_service', 'apply_theme', 'get_theme_manager'] 
